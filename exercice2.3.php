b8ffe21f7d178b0f13ee5f1603885135b9adb56b
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 15:15:24 +0200

    correction ajout HTML

--- exercice2.3.php
+++ exercice2.3.php
@@ -1,20 +1,34 @@
-<?php
-$age = 89;
-$gender = "femme";
-if ($age >=18 AND $gender = "femme")
-{
-    echo "Vous êtes une femme et vous êtes majeure";
-}
-elseif ($age < 18 AND $gender = "femme")
-{
-    echo "Vous êtes une femme et vous êtes mineures";
-}
-elseif ($age >=18 AND $gender = "homme")
-{
-    echo "Vous êtes un homme et vous êtes majeur";
-}
-elseif ($age <18 AND $gender = "homme")
-{
-    echo "Vous êtes un homme et vous êtes mineur";
-}
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 2.3</title>
+</head>
+<body>
+    <h1>Exercice 3 Partie 2 : Les conditions PHP</h1>
+    <p>
+        <?php
+            $age = 89;
+            $gender = "femme";
+            if ($age >=18 AND $gender = "femme")
+            {
+                echo "Vous êtes une femme et vous êtes majeure";
+            }
+            elseif ($age < 18 AND $gender = "femme")
+            {
+                echo "Vous êtes une femme et vous êtes mineures";
+            }
+            elseif ($age >=18 AND $gender = "homme")
+            {
+                echo "Vous êtes un homme et vous êtes majeur";
+            }
+            elseif ($age <18 AND $gender = "homme")
+            {
+                echo "Vous êtes un homme et vous êtes mineur";
+            }
+        ?>
+    </p>
+</body>
+</html>
